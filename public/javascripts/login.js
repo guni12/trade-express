995bc072e320e759d59fa8464ea4d5265ca7170d
Gunvor Nilsson <gunvor@behovsbo.se>
2019-03-24 20:53:15 +0000

    starting integration-chain

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -90,46 +90,7 @@ module.exports = (function () {
     }
 
 
-
-
-    function checkToken(req, res, next) {
-        var token = req.headers['x-access-token'];
-
-        //console.log(req.headers, token);
-
-        if (token) {
-            jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            status: 500,
-                            source: req.path,
-                            title: "Failed authentication",
-                            detail: err.message
-                        }
-                    });
-                }
-
-                req.user = {};
-                req.user.token = decoded.token;
-                req.user.email = decoded.email;
-
-                next();
-            });
-        } else {
-            return res.status(401).json({
-                errors: {
-                    status: 401,
-                    source: req.path,
-                    title: "No token",
-                    detail: "No token provided in request headers"
-                }
-            });
-        }
-    }
-
     return {
-        login: login,
-        checkToken: checkToken
+        login: login
     };
 }());
---
+++ test/javascripts/index/index_integration.js
@@ -0,0 +1,58 @@
+/**
+ * Test for class Card.
+ */
+"use strict";
+
+/* global describe it */
+const me = require("../../../public/javascripts/index");
+var assert = require("assert");
+
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../app.js');
+
+//console.log(server);
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("Test something from our homepage", function() {
+    describe("We test the info function.", function() {
+        it("info.header should be Front", function() {
+            var info = me.info.find();
+            let res = info[0].header;
+
+            assert.equal(res, "Front");
+        });
+
+        it("info.description should be array", function() {
+            let info = me.info.find();
+            let res = info[0].description[0];
+
+            assert.equal(res, "In ten years time 100TWh fossile power, now used to drive vehicle, will be replaced by 30TWh electric power.");
+        });
+    });
+});
+
+
+describe('Index new model', () => {
+    describe('GET /index', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("array");
+                    res.body.length.should.be.above(0);
+                    res.body[0].should.be.an("object");
+                    res.body[0].header.should.be.a("string");
+
+                    done();
+                });
+        });
+    });
+});
